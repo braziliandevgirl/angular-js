6badec94018565823a827e5c47bf6ffe674df270
JOLIVEIR <janile.dantas@sbi-technology.com>
2020-05-03 20:13:01 -0300

    7-Precisamos alimentar esse sistema-Validando nosso form

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,10 +1,22 @@
 angular.module('alurapic')
-    .controller('FotoController', function($scope) {
+    .controller('FotoController', function($scope, $http) {
 
         $scope.foto = {};
+        $scope.mensagem = {};
 
-        $scope.submeter = function() {
-            console.log($scope.foto);
+        $scope.submeter = function() {         
+            if($scope.formulario.$valid) {
+                $http.post('v1/fotos', $scope.foto)
+                .success(function() {
+                    $scope.foto = {};
+                    $scope.mensagem = 'Foto incluída com sucesso';
+                    console.log('Foto cadastrada com sucesso.')
+                })
+                .error(function(erro) {
+                    $scope.mensagem = 'Nao foi possivel incluir a foto';
+                    console.log(erro);
+                });
+            }  
         };
 
     });
