d3ac14ceeec3abdff508705fc8cc598ef78208bd
JOLIVEIR <janile.dantas@sbi-technology.com>
2020-05-03 22:16:21 -0300

    8-Dados inconsistentes? Precisamos removê-los ou alterá-los!

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,21 +1,44 @@
 angular.module('alurapic')
-    .controller('FotoController', function($scope, $http) {
+    .controller('FotoController', function($scope, $http, $routeParams) {
 
         $scope.foto = {};
         $scope.mensagem = {};
 
+        if($routeParams.fotoId) {
+            $http.get('v1/fotos/' + $routeParams.fotoId)
+            .success(function(foto) {
+                $scope.foto = foto;
+            })
+            .error(function(erro) {
+                console.log(erro);
+                $scope.mensagem = 'Nao foi possivel obter a foto';
+            });
+        };        
+
         $scope.submeter = function() {         
             if($scope.formulario.$valid) {
-                $http.post('v1/fotos', $scope.foto)
-                .success(function() {
-                    $scope.foto = {};
-                    $scope.mensagem = 'Foto incluída com sucesso';
-                    console.log('Foto cadastrada com sucesso.')
-                })
-                .error(function(erro) {
-                    $scope.mensagem = 'Nao foi possivel incluir a foto';
-                    console.log(erro);
-                });
+                if($scope.foto._id) {
+                    $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+                    .success(function() {
+                        $scope.mensagem = 'A foto ' + $scope.foto.titulo + ' foi alterada com sucesso.';
+                    })
+                    .error(function(erro) {
+                        console.log(erro);
+                        $scope.mensagem = 'Nao foi possivel alterar';
+                    });                    
+                }
+                else {
+                    $http.post('v1/fotos', $scope.foto)
+                    .success(function() {
+                        $scope.foto = {};
+                        $scope.mensagem = 'Foto incluída com sucesso';
+                        console.log('Foto cadastrada com sucesso.')
+                    })
+                    .error(function(erro) {
+                        $scope.mensagem = 'Nao foi possivel incluir a foto';
+                        console.log(erro);
+                    });
+                }
             }  
         };
 
